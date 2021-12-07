37b792acfa027565b261b2014ff7899e668b792e
Eugenia Khaleeva <khaleeva20990@gmail.com>
2021-12-08 02:27:37 +0300

    add table  to calculator

--- js/script.js
+++ js/script.js
@@ -253,8 +253,6 @@ for (let link of reviewLinks) {
 
 ;
 
-
-
 const ranges = document.querySelectorAll('.range')
 const calcBtnUnit = document.querySelector("#btn-unit");
 const calcBtnTower = document.querySelector("#btn-tower");
@@ -263,6 +261,9 @@ const checkBox = document.querySelector('.checkbox-input');
 const currentCost = document.querySelector(".cost-info");
 const inputIp = document.querySelector('#input-ip');
 const swipeCalcBtn = document.querySelector("#calc-swipe-line");
+const addCalcBtn = document.querySelector('.btn-add-calc');
+const calcTable = document.querySelector('.calc-table');
+const costCalc = document.querySelector('.cost-calculation');
 
 
 let measure;
@@ -272,6 +273,8 @@ let unitValue = 0;
 let portValue = 0;
 let ipValue = 0;
 let supplyValue = 0;
+let sum = 0;
+
 let costServer = +document.querySelector('#costServer').innerHTML.replace(/,/, '.');
 let costTower = +document.querySelector('#costTower').innerHTML.replace(/,/, '.');
 const costPower = +document.querySelector('#costPower').innerHTML.replace(/,/, '.');
@@ -279,42 +282,45 @@ const costUnit = +document.querySelector('#costUnit').innerHTML.replace(/,/, '.'
 const costSupply = +document.querySelector('#costSupply').innerHTML.replace(/,/, '.');
 const costPort = +document.querySelector('#costPort').innerHTML.replace(/,/, '.');
 const costIp = +document.querySelector('#costIp').innerHTML.replace(/,/, '.');
-let sum = 0;
+const IP_REGEXP = /^\d{1,4}$/;
 
 
+function validateIpInput(value) {
+  return IP_REGEXP.test(value);
+}
 
 for(let range of ranges){
-  range.oninput = function(){
+    range.oninput = function(){
     output = this.parentElement.parentElement.firstElementChild.lastElementChild;
     measure = output.getAttribute('data-value');
     output.innerHTML = `${this.value} ${measure}` ;
     
   }
 
-  range.addEventListener('input', function(){
-    let x = 0;
-    if (measure === 'Вт'){
-      x = (this.value/12.5)-20;
-      this.previousSibling.previousSibling.firstElementChild.style.width = x + '%';
-      powerValue = parseFloat((((this.value-250)/50) * costPower).toFixed(2));
-      console.log(powerValue);
+    range.addEventListener('input', function(){
+      let x = 0;
+      if (measure === 'Вт'){
+        x = (this.value/12.5)-20;
+        this.previousSibling.previousSibling.firstElementChild.style.width = x + '%';
+        powerValue = parseFloat((((this.value-250)/50) * costPower).toFixed(2));
+        console.log(powerValue);
+        
+      } else if (measure === 'U'){
+        x = (this.value*14.286)-14.286;
+        this.previousSibling.previousSibling.firstElementChild.style.width = x + '%';
+        unitValue = parseFloat(((this.value * costUnit)-costUnit).toFixed(2));
+        console.log(unitValue);
+        
+      } else if (measure === 'Gb/s'){
+        x = (this.value*14.286)-14.286;
+        this.previousSibling.previousSibling.firstElementChild.style.width = x + '%';
       
-    } else if (measure === 'U'){
-      x = (this.value*14.286)-14.286;
-      this.previousSibling.previousSibling.firstElementChild.style.width = x + '%';
-      unitValue = parseFloat(((this.value * costUnit)-costUnit).toFixed(2));
-      console.log(unitValue);
-      
-    } else if (measure === 'Gb/s'){
-      x = (this.value*14.286)-14.286;
-      this.previousSibling.previousSibling.firstElementChild.style.width = x + '%';
-     
-      portValue = parseFloat(((this.value * costPort)-costPort).toFixed(2));
-      console.log(portValue);
-    } 
+        portValue = parseFloat(((this.value * costPort)-costPort).toFixed(2));
+        console.log(portValue);
+      } 
 
       calcValue();
-  })
+    })
 }
 
 
@@ -328,121 +334,174 @@ document.querySelector('.checkbox-container').addEventListener("change", functio
       supplyValue = 0;
     }
 
-    calcValue();
+  calcValue();
   
 })
 
 
 inputIp.oninput = function() {
 
-  
-   
   let currentValue = 0;
 
-  if(inputIp.value <= 1){
-    currentValue = 0;
-    ipValue = 0;
-
+  if(validateIpInput(inputIp.value)){
+    document.querySelector('.ip-span').classList.remove('active-span');
+    if(inputIp.value <= 1){
+      currentValue = 0;
+      ipValue = 0;
+      
+    } else {
+      currentValue = inputIp.value - 1;
+      ipValue = parseFloat((currentValue * costIp).toFixed(2));
+    }
+  
+    
+    calcValue();
   } else {
-    currentValue = inputIp.value - 1;
-    ipValue = parseFloat((currentValue * costIp).toFixed(2));
+    document.querySelector('.ip-span').classList.add('active-span');
   }
 
   
-  calcValue();
 };
 
 
-
 function calcValue(){
-   
-   sum = parseFloat(costServer + powerValue + unitValue + ipValue + portValue + supplyValue).toFixed(2);
+   let cost;
+  if(costCalc.querySelector('.unit-item').style.display === 'block'){
+    cost = costServer;
+  } else cost = costTower;
+
+  sum = parseFloat(cost + powerValue + unitValue + ipValue + portValue + supplyValue).toFixed(2);
   currentCost.innerHTML = `${sum} BYN`;
+  return sum;
+}
+
+addCalcBtn.addEventListener('click', function(){
+  document.querySelector('.calc-container').classList.add('active-calc-container');
+  calcTable.classList.add('active-calc-table');
   
+})
+
+addCalcBtn.addEventListener('click', addCalcServer);
+
+
+function addCalcServer () {
+
+  let total = 0;
+
+  if(calcTable.querySelectorAll('.calc-table-container').length >=9){
+    addCalcBtn.disabled = 'true';
+    addCalcBtn.classList.remove('btn-gr-bg');
+    addCalcBtn.classList.add('disabled');
+  }
+
+  
+  
+  let calcTableContainer = document.createElement('div');
+  calcTableContainer.classList.add('calc-table-container');
+  calcTable.prepend(calcTableContainer);
+   
+  let calcTableName = document.createElement('div');
+  calcTableName.classList.add('calc-table-name');
+  calcTableName.innerHTML = 'Сервер';
+  calcTableContainer.append(calcTableName);
+   
+  let calcTableCost = document.createElement('div');
+  calcTableCost.classList.add('calc-table-cost');
+  calcTableCost.innerHTML = calcValue();
+  calcTableContainer.append(calcTableCost);
+  nullCalcValue();
+
+  for(let elem of document.querySelectorAll('.calc-table-cost')){
+    total = total + parseFloat(elem.innerHTML);
+  }
+  
+
+  document.querySelector('.calc-total-amount').style.display = 'flex';
+  document.querySelector('.total').innerHTML = `${total} BYN`;
+ 
 }
 
 
 
 
 
-    calcBtnUnit.addEventListener('click', function(){
-        document.body.classList.add('active-body');
-        document.querySelector('.wrapper-calc').classList.add('active')
-        document.querySelector('.unit-item').style.display = 'block';
-        currentCost.innerHTML = `${costServer}.00 BYN`;
-        document.querySelector('.calc-form').classList.add('active-calc');
+  
+
+calcBtnUnit.addEventListener('click', function(){
+  document.body.classList.add('active-body');
+  document.querySelector('.wrapper-calc').classList.add('active')
+  document.querySelector('.unit-item').style.display = 'block';
+  document.querySelector('.calc-container').classList.add('active-calc');
+  currentCost.innerHTML = `${costServer}.00 BYN`;
+  
+})
+
+
+calcBtnTower.addEventListener('click', function(){
+  document.body.classList.add('active-body');
+  document.querySelector('.wrapper-calc').classList.add('active');
+  document.querySelector('.calc-container').classList.add('active-calc');
+  document.querySelector('.unit-item').style.display = 'none';
+  currentCost.innerHTML = `${costTower}.00 BYN`;
+  
+})
+
+
+function nullCalcValue(){
+  for(range of ranges){
+    range.value = 0;
+    range.previousSibling.previousSibling.firstElementChild.style.width = 0 + '%';
+    
+    if (range.parentElement.parentElement.firstElementChild.lastElementChild.getAttribute('data-value') === 'Вт'){
+      range.parentElement.parentElement.firstElementChild.lastElementChild.innerHTML = `250 Вт`;
       
-        
-    })
+    } else if (range.parentElement.parentElement.firstElementChild.lastElementChild.getAttribute('data-value') === 'U'){
+      range.parentElement.parentElement.firstElementChild.lastElementChild.innerHTML = `1 U`;
+    } else {
+      range.parentElement.parentElement.firstElementChild.lastElementChild.innerHTML = `1 Gb/s`;
+    }
 
+    powerValue = 0;
+    unitValue = 0;
+    ipValue = 0;
+    portValue = 0;
+    supplyValue = 0;
+    inputIp.value = '';
+  }
 
+  if(checkBox.checked){
+    checkBox.checked = false;
+  }
 
+  if(costCalc.querySelector('.unit-item').style.display === 'block'){
+    currentCost.innerHTML = `${costServer}.00 BYN`; 
+  
+  } else currentCost.innerHTML = `${costTower}.00 BYN`;
+  
 
-
-
-    calcBtnTower.addEventListener('click', function(){
-      document.body.classList.add('active-body');
-      document.querySelector('.wrapper-calc').classList.add('active');
-      document.querySelector('.calc-form').classList.add('active-calc');
-      document.querySelector('.unit-item').style.display = 'none';
-      // unitValue = 0;
-      currentCost.innerHTML = `${costTower}.00 BYN`;
-      costServer = costTower;
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 function nullValue(){
   document.body.classList.remove('active-body');
-    document.querySelector('.wrapper-calc').classList.remove('active');
-    sum = 0;
-    currentCost.innerHTML = `${sum} BYN`;
-    console.log(currentCost);
-    for(range of ranges){
-      range.value = 0;
-      range.previousSibling.previousSibling.firstElementChild.style.width = 0 + '%';
-      
-      if (range.parentElement.parentElement.firstElementChild.lastElementChild.getAttribute('data-value') === 'Вт'){
-        range.parentElement.parentElement.firstElementChild.lastElementChild.innerHTML = `250 Вт`;
-        
-      } else if (range.parentElement.parentElement.firstElementChild.lastElementChild.getAttribute('data-value') === 'U'){
-        range.parentElement.parentElement.firstElementChild.lastElementChild.innerHTML = `1 U`;
-      } else {
-        range.parentElement.parentElement.firstElementChild.lastElementChild.innerHTML = `1 Gb/s`;
-      }
-
-      powerValue = 0;
-      unitValue = 0;
-      ipValue = 0;
-      portValue = 0;
-      supplyValue = 0;
-      inputIp.value = '';
+  document.querySelector('.calc-container').classList.remove('active-calc-container');
+  document.querySelector('.wrapper-calc').classList.remove('active');
+  calcTable.classList.remove('active-calc-table');
+   
+  nullCalcValue();
+    
+  if(calcTable.querySelectorAll('.calc-table-container').length > 0){
+    for(child of calcTable.querySelectorAll('.calc-table-container')){
+        child.remove();
     }
+     
+  }
+  
+  addCalcBtn.disabled = false;
+  addCalcBtn.classList.add('btn-gr-bg');
+  addCalcBtn.classList.remove('disabled');
+  document.querySelector('.ip-span').classList.remove('active-span');
 
-    
-    
-    // costServer = +document.querySelector('#costServer').innerHTML.replace(/,/, '.');
-    
-
-      if(checkBox.checked){
-        checkBox.checked = false;
-      }
 }
 
 
