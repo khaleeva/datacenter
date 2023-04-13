5525cc12cf60fe6edac8aea3c1542e31f524f0ad
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-04-14 01:30:23 +0200

    refactor: refactor map page

--- datacenter-project/#src/js/map.js
+++ datacenter-project/#src/js/map.js
@@ -1,32 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    lazyLoadMaps()
+})
 
-let map_containers = document.querySelectorAll('.map_container');
-let options_map = {
-    once: true,
-    passive: true,
-    capture: true
-};
 
-if (map_containers) {
-    for (let map_container of map_containers) {
-        map_container.addEventListener('click', start_lazy_map, options_map);
-        map_container.addEventListener('mouseover', start_lazy_map, options_map);
-        map_container.addEventListener('touchstart', start_lazy_map, options_map);
-        map_container.addEventListener('touchmove', start_lazy_map, options_map);
-    }
+function lazyLoadMaps(){
+    let maps = document.querySelectorAll('.map__card');
+    let options_map = {
+        once: true,
+        passive: true,
+        capture: true
+    };
 
-}
+    maps.forEach(map => {
+        map.addEventListener('click', start_lazy_map, options_map);
+        map.addEventListener('mouseover', start_lazy_map, options_map);
+        map.addEventListener('touchstart', start_lazy_map, options_map);
+        map.addEventListener('touchmove', start_lazy_map, options_map);
+    });
 
-let map_loaded = false;
-function start_lazy_map() {
-    if (!map_loaded) {
-        let map_blocks = document.querySelectorAll('.ymap_lazy');
-        for (let map_block of map_blocks) {
-            map_loaded = true;
-            map_block.setAttribute('src', map_block.getAttribute('data-src'));
-            map_block.removeAttribute('data-src');
+    let map_loaded = false;
+    function start_lazy_map() {
+        if (!map_loaded) {
+            let ymaps_lazy = document.querySelectorAll('.ymap_lazy');
+            ymaps_lazy.forEach(ymap_lazy => {
+                map_loaded = true;
+                ymap_lazy.setAttribute('src', ymap_lazy.getAttribute('data-src'));
+                ymap_lazy.removeAttribute('data-src');
+            })
         }
-
     }
 }
+
+
 
 
