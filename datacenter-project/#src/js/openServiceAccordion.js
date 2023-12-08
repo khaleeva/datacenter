a2e4fc32ef5f49406923e0b8ecdf4dea820861b9
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-12-08 14:23:30 +0100

    fix: fix open acc

--- datacenter-project/#src/js/openServiceAccordion.js
+++ datacenter-project/#src/js/openServiceAccordion.js
@@ -1,50 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
-    if (document.querySelector('.all-services__accordion')) {
-        openAccordionService()
-    }
+	if (document.querySelector('.all-services__accordion')) {
+		openAccordionService()
+	}
 
 });
 
 function openAccordionService() {
 
-    let firstOpenAccordionBody = document.querySelectorAll('.first-open-body');
-    const accordion_container = document.querySelector('.all-services__accordion');
-    const accordion_headers = accordion_container.querySelectorAll('.accordion__header');
-    const accordion_bodies = accordion_container.querySelectorAll('.accordion__body');
+	let firstOpenAccordionBody = document.querySelectorAll('.first-open-body');
+	const accordion_container = document.querySelector('.all-services__accordion');
+	const accordion_headers = accordion_container.querySelectorAll('.accordion__header');
+	const accordion_bodies = accordion_container.querySelectorAll('.accordion__body');
 
-    firstOpenAccordionBody.forEach(first => {
-        first.style.maxHeight = first.scrollHeight + 'px';
-    })
+	firstOpenAccordionBody.forEach(first => {
+		first.style.maxHeight = first.scrollHeight + 'px';
+	})
 
 
-    accordion_headers.forEach(header => header.addEventListener('click', (e) => {
-        if (e.target.classList.contains('button__arrow')) {
-            accordion_headers.forEach(h => {
-                if (h !== header) {
-                    h.querySelector('.button__arrow').classList.remove('button__arrow_rotate');
-                }
-            });
+	accordion_headers.forEach(header => header.addEventListener('click', (e) => {
 
-            firstOpenAccordionBody.forEach(first => {
-                first.classList.remove('first-open-body')
-            })
+		firstOpenAccordionBody.forEach(first => {
+			first.classList.remove('first-open-body')
+		})
+		let current_body = header.nextElementSibling;
 
-            e.target.classList.toggle('button__arrow_rotate');
-            let current_body = header.nextElementSibling;
+		if (current_body.style.maxHeight) {
+			current_body.style.maxHeight = null;
+			current_body.previousElementSibling.querySelector('.button__arrow').classList.remove('button__arrow_rotate')
+		} else {
+			accordion_bodies.forEach(body => {
+				if (body.style.maxHeight) {
+					body.style.maxHeight = null;
+					body.previousElementSibling.querySelector('.button__arrow').classList.remove('button__arrow_rotate')
+				}
+			})
+			current_body.style.maxHeight = current_body.scrollHeight + 'px';
+			current_body.previousElementSibling.querySelector('.button__arrow').classList.add('button__arrow_rotate')
+		}
 
-            if (current_body.style.maxHeight) {
-                current_body.style.maxHeight = null;
-            } else {
-                accordion_bodies.forEach(body => {
-                    if (body.style.maxHeight) {
-                        body.style.maxHeight = null;
-                    }
-                })
-                current_body.style.maxHeight = current_body.scrollHeight + 'px';
-            }
 
-        }
-    }))
+	}))
 };
 
 
