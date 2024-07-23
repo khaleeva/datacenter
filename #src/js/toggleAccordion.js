7d4003a5244f228c45beac7150a90e30d1124d02
eugeniakhaleeva <khaleeva20990@gmail.com>
2024-07-23 14:26:44 +0200

    refactor: fix adaptive

--- #src/js/toggleAccordion.js
+++ #src/js/toggleAccordion.js
@@ -1,24 +1,28 @@
 function toggleAccordion() {
-    const accordion_headers = document.querySelectorAll(".accordion__header");
-    const burger = document.querySelector('.burger');
-    const accordion_bodies = document.querySelectorAll('.accordion__body');
+  const accordionHeaders = document.querySelectorAll(".accordion__header");
+  const burger = document.querySelector(".burger");
+  const accordionBodies = document.querySelectorAll(".accordion__body");
 
-    accordion_headers.forEach(header => header.addEventListener('click', function (e) {
-            const current_header = e.target;
-            const current_panel = current_header.nextElementSibling;
-            if(burger.classList.contains('burger_open')){
-                if(current_panel.classList.contains("accordion__body_open")){
-                    current_panel.classList.remove("accordion__body_open")
-                } else {
-                    accordion_bodies.forEach(body => {
-                        if(body.classList.contains("accordion__body_open")){
-                            body.classList.remove("accordion__body_open")
-                        }
-                    })
-                    current_panel.classList.add("accordion__body_open")
-                }
-            }
+  accordionHeaders.forEach((header) => {
+    header.addEventListener("click", function () {
+      const currentHeader = header;
+      const currentPanel = currentHeader.nextElementSibling;
 
-    }))
-
-};
+      if (burger.classList.contains("burger_open")) {
+        if (currentPanel.classList.contains("accordion__body_open")) {
+          currentPanel.classList.remove("accordion__body_open");
+          currentHeader.classList.remove("active");
+        } else {
+          accordionBodies.forEach((body) => {
+            body.classList.remove("accordion__body_open");
+          });
+          accordionHeaders.forEach((header) => {
+            header.classList.remove("active");
+          });
+          currentPanel.classList.add("accordion__body_open");
+          currentHeader.classList.add("active");
+        }
+      }
+    });
+  });
+}
